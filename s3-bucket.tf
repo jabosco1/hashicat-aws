38f104b6d775920bbd9589042733e5c0e77b7e93
John Bosco <john.bosco@peoples.com>
2021-03-30 18:31:22 +0000

    Added s3 bucket module 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/pub-jabosco/s3-bucket/aws"
   version = "1.15.0"
   # insert required variables here
-  bucket_prefix = "pub-jabosco"  
+  bucket_prefix = var.prefix 
 }
